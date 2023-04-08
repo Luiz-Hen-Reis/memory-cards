18193e0e05947fed48458ce11c1d0c9aa85b259f
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-04-08 19:16:23 -0300

    Initial commit from Create Next App

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Index() {
+  return (
+    <>
+      <Head>
+        <title>Novo Projeto</title>
+      </Head>
+      <div>Bora Codar?</div>
+    </>
+  )
+}
+
+export default Index;
